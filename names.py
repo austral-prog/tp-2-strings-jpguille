5e890d20d453b2594f17f6880d3bf9be55986a5b
jpguille <jpguillen2008@gmail.com>
2026-03-17 20:50:24 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,4 +2,11 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
-    pass
+    nombre = input("Introduzca su nombre: ")
+    apellido = input("Introduzca su apellido: ")
+    nombre_completo = nombre + " " + apellido
+    print(nombre_completo.lower())
+    print(nombre_completo.title())
+    print(nombre_completo.upper())
+    print("\t" + nombre_completo.lower())
+
